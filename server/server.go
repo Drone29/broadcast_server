3527f6d6cd53cea1945c695ba5d6451c4a8ae763
andrey_dergunov <andrei_dergunov@epam.com>
2025-02-17 15:59:55 +0100

    server logic draft

--- server/server.go
+++ server/server.go
@@ -3,47 +3,85 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
-func handleReceivedMsg(ws_conn *websocket.Conn, msgType int, msg []byte) {
+var (
+	// list of unique clients
+	clients     map[*websocket.Conn]struct{}
+	clients_mtx = sync.Mutex{}
+	ws_upgrader = websocket.Upgrader{}
+)
 
-}
-
-// main handler
-func handleWS(w http.ResponseWriter, r *http.Request) {
-	upgrader := websocket.Upgrader{
+// initialize variables
+func init() {
+	clients = make(map[*websocket.Conn]struct{})
+	ws_upgrader = websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool {
+			//todo: check auth
 			return true // allow all connections
 		},
 	}
+}
 
-	conn, err := upgrader.Upgrade(w, r, nil)
+func addClientToList(conn *websocket.Conn) {
+	clients_mtx.Lock()
+	defer clients_mtx.Unlock()
+	clients[conn] = struct{}{}
+}
+
+func disconnectAndRemoveClientFromList(conn *websocket.Conn) {
+	clients_mtx.Lock()
+	defer clients_mtx.Unlock()
+	conn.Close()
+	delete(clients, conn)
+}
+
+func broadcastMessage(msgType int, msg []byte) {
+	clients_mtx.Lock()
+	defer clients_mtx.Unlock()
+	for conn := range clients {
+		err := conn.WriteMessage(msgType, msg)
+		if err != nil {
+			fmt.Println("WS error sending message to:", conn.RemoteAddr())
+			// remove dead connection
+			disconnectAndRemoveClientFromList(conn)
+		}
+	}
+}
+
+// main handler
+func handleWSConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := ws_upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("WS upgrade error:", err)
 		return
 	}
-	defer conn.Close()
 
-	fmt.Println("WS new client connected")
+	fmt.Println("WS new client connected:", conn.RemoteAddr())
+	// store client in the set
+	addClientToList(conn)
 
 	for {
 		msgType, msg, err := conn.ReadMessage()
 		if err != nil {
-			fmt.Println("WS Read error:", err)
+			fmt.Println("WS read error:", conn.RemoteAddr(), err)
+			disconnectAndRemoveClientFromList(conn)
+			break
 		}
 
-		fmt.Printf("WS received %v %s\n", msgType, msg)
+		fmt.Printf("WS received from %s %v %v\n", conn.RemoteAddr(), msgType, msg)
 
 		// handle received message
-		handleReceivedMsg(conn, msgType, msg)
+		broadcastMessage(msgType, msg)
 	}
 }
 
 // start server
 func Start(port int) {
-	http.HandleFunc("/ws", handleWS)
+	http.HandleFunc("/ws", handleWSConnection)
 	fmt.Println("WS server started on port", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
