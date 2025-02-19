e1d9ca719ebf992e14aa515f1cd55852d1a40b67
andrey_dergunov <andrei_dergunov@epam.com>
2025-02-19 15:15:22 +0100

    rework to use channels, no mutexes

--- server/server.go
+++ server/server.go
@@ -8,126 +8,115 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var (
+type WSMessage struct {
+	MsgType int
+	Content []byte
+}
+
+type WSServer struct {
 	// list of unique clients
-	clients     map[*WSClient]bool
-	clients_mtx = sync.RWMutex{}
-	ws_upgrader = websocket.Upgrader{}
-)
-
-// initialize variables
-func init() {
-	clients = make(map[*WSClient]bool)
-	ws_upgrader = websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			//todo: check auth
-			return true // allow all connections
-		},
-	}
+	clients      map[*websocket.Conn]bool
+	broadcast_q  chan WSMessage
+	register_q   chan *websocket.Conn
+	unregister_q chan *websocket.Conn
+	upgrader     websocket.Upgrader
 }
 
-func addClientToList(conn *WSClient) {
-	clients_mtx.Lock()
-	defer clients_mtx.Unlock()
-	clients[conn] = true
-}
+const CLIENTS_BUFFER_SIZE = 10
 
-func closeSingleConnection(conn *WSClient) {
-	deadConns := make(chan *WSClient, 1)
-	deadConns <- conn
-	close(deadConns)
-	cleanupDeadConnections(deadConns)
-}
-
-func cleanupDeadConnections(deadConns <-chan *WSClient) {
-	clients_mtx.Lock()
-	defer clients_mtx.Unlock()
-
-	for conn := range deadConns {
-		fmt.Println("WS closing", conn.RemoteAddr())
-		conn.Close()
-		delete(clients, conn)
-	}
-}
-
-func getClients() []*WSClient {
-	clients_mtx.RLock()
-	defer clients_mtx.RUnlock()
-	// copy to a slice to ensure we're operating on a stable list of connections
-	connections := make([]*WSClient, 0, len(clients))
-	for conn := range clients {
-		connections = append(connections, conn)
-	}
-	return connections
-}
-
-func broadcastMessage(msgType int, msg []byte) <-chan *WSClient {
-	var wg sync.WaitGroup
-	connections := getClients()
-	// create a buffered channel for dead connections
-	deadConns := make(chan *WSClient, len(connections))
-
-	message, err := PrepareMessage(msgType, msg)
+func (s *WSServer) broadcast_message(msg WSMessage) error {
+	message, err := websocket.NewPreparedMessage(msg.MsgType, msg.Content)
 	if err != nil {
 		fmt.Println("WS error preparing message", err)
-		return deadConns
+		return err
 	}
+	var wg sync.WaitGroup
 
-	for _, conn := range connections {
+	for conn := range s.clients {
 		wg.Add(1) // increment semaphore
 		// execute in a goroutine
-		go func(c *WSClient) {
+		go func(c *websocket.Conn) {
 			defer wg.Done() // decrement semaphore
 			if err := c.WritePreparedMessage(message); err != nil {
 				fmt.Println("WS error sending message to:", c.RemoteAddr())
 				// append dead connection to buffered channel
-				deadConns <- c
+				s.unregister_q <- c
 			}
 		}(conn)
 	}
 
-	// wait until all messages are sent to all clients and close the channel
-	go func() {
-		wg.Wait()
-		close(deadConns)
-	}()
+	wg.Wait() // wait for all goroutines to complete
 
-	return deadConns
+	return nil
+}
+
+func (s *WSServer) run() {
+	for {
+		select {
+		case conn := <-s.register_q:
+			s.clients[conn] = true
+		case conn := <-s.unregister_q:
+			if _, ok := s.clients[conn]; ok {
+				conn.Close()
+				delete(s.clients, conn)
+			}
+		case msg := <-s.broadcast_q:
+			s.broadcast_message(msg)
+		}
+	}
+}
+
+func NewWSServer() *WSServer {
+	return &WSServer{
+		clients:      make(map[*websocket.Conn]bool),
+		broadcast_q:  make(chan WSMessage, CLIENTS_BUFFER_SIZE),
+		register_q:   make(chan *websocket.Conn, CLIENTS_BUFFER_SIZE),
+		unregister_q: make(chan *websocket.Conn, CLIENTS_BUFFER_SIZE),
+		upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				//todo: check auth
+				return true // allow all connections
+			},
+		},
+	}
 }
 
 // main handler
-func handleWSConnection(w http.ResponseWriter, r *http.Request) {
-	conn, err := ws_upgrader.Upgrade(w, r, nil)
+func (s *WSServer) handleWSConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := s.upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("WS upgrade error:", err)
 		return
 	}
 
-	client := NewWSClient(conn)
-
 	fmt.Println("WS new client connected:", conn.RemoteAddr())
-	// store client in the set
-	addClientToList(client)
+	// enqueue new client
+	s.register_q <- conn
 
 	for {
 		msgType, msg, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Println("WS client error:", conn.RemoteAddr(), err)
-			closeSingleConnection(client)
+			// enqueue client to be unregistered
+			s.unregister_q <- conn
 			break
 		}
 
 		fmt.Printf("WS received from %s %s\n", conn.RemoteAddr(), msg)
 
-		// handle received message
-		deadConns := broadcastMessage(msgType, msg)
-		cleanupDeadConnections(deadConns)
+		// enqueue message for broadcasting
+		s.broadcast_q <- WSMessage{MsgType: msgType, Content: msg}
 	}
 }
 
 // start server
 func Start(port int) {
-	http.HandleFunc("/ws", handleWSConnection)
+	ws_server := NewWSServer()
+
+	// run server
+	go ws_server.run()
+
+	http.HandleFunc("/ws", ws_server.handleWSConnection)
 	fmt.Println("WS server started on port", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
