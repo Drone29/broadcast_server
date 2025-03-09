ede75df30ee377cb2f374997aeebbf9d187977ee
andrey_dergunov <andrei_dergunov@epam.com>
2025-03-09 16:52:27 +0100

    client: better handling for sudden server turndowns

--- client/client.go
+++ client/client.go
@@ -27,6 +27,8 @@ func (c *Client) receiveMessages() {
 		msg_type, msg, err := c.conn.ReadMessage()
 		if err != nil {
 			fmt.Println("WS reading error", c.conn.RemoteAddr(), err)
+			close(c.msg_rx_q) // close channel and exit
+			return
 		} else {
 			// send to queue
 			c.msg_rx_q <- wsMessage{
@@ -45,6 +47,8 @@ func (c *Client) sendMessages() {
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("error reading input:", err)
+			close(c.msg_tx_q) //close channel and exit
+			return
 		} else {
 			// cut \n from input
 			input = input[:len(input)-1]
@@ -62,25 +66,24 @@ func (c *Client) handleMessages() {
 	go c.receiveMessages()
 	go c.sendMessages()
 
-	active := true
-	closeConnection := func() {
-		fmt.Println("closing WS connection...")
-		active = false
-		c.conn.Close()
-	}
 	// handle received/to-be-sent messages
-	for active {
+	for {
 		select {
-		case msg := <-c.msg_rx_q:
-			//print received message to stdout
-			fmt.Printf("Received message: %s\n", string(msg.data))
-		case msg := <-c.msg_tx_q:
-			// send message from stdin to server
-			if err := c.conn.WriteMessage(msg.msg_type, msg.data); err != nil {
-				fmt.Println("error sending message:", err)
+		case msg, ok := <-c.msg_rx_q:
+			if ok {
+				//print received message to stdout
+				fmt.Printf("Received message: %s\n", string(msg.data))
+			}
+		case msg, ok := <-c.msg_tx_q:
+			if ok {
+				// send message from stdin to server
+				if err := c.conn.WriteMessage(msg.msg_type, msg.data); err != nil {
+					fmt.Println("error sending message:", err)
+				}
 			}
 		case <-c.quit:
-			closeConnection()
+			c.conn.Close()
+			return
 		}
 	}
 }
@@ -101,6 +104,6 @@ func Connect(url string) Client {
 }
 
 func (c *Client) Disconnect() {
-	close(c.quit) //close channel to signal routine to exit
+	close(c.quit) //signal shutdown
 	fmt.Println("Disconnecting WS client...")
 }
